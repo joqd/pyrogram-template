49ba13dcfef52795f38809243854bbae52f0a550
root <root@tunel.aeza.network>
2023-12-28 19:02:49 +0300

    model base; without methods

--- models.py
+++ models.py
@@ -7,9 +7,8 @@ db.bind(provider='sqlite', filename='../db.sqlite3', create_db=True)
 
 
 class User(db.Entity):
-    id        = orm.PrimaryKey(str) # user_id, ex: 956473054
-    name      = orm.Required(str)
-    timestamp = orm.Required(datetime)
+    id        = orm.PrimaryKey(str)
+    joined_at = orm.Required(datetime)
 
 
 db.generate_mapping(create_tables=True)
